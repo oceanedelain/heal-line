ee131863713b39e0cb9e387a77bf7c984adcbc89
Oceane Delain <delain.oceane@gmail.com>
2021-05-24 18:18:15 +0200

    fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :consultations,
+  has_many :consultations
   has_many :doctors, through: :consultations
 end
