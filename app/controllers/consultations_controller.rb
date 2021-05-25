c4981fc471312e44d664bcbf041e26f588665eac
Emmanuel BIRABEN <ebiraben14@hotmail.com>
2021-05-25 15:31:41 +0200

    pundit MAJ

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,7 +1,10 @@
 class ConsultationsController < ApplicationController
 
-
   def new
     @consultation = Consultation.new
+  end
+
+  def create
+
   end
 end
---
+++ app/policies/consultation_policy.rb
@@ -0,0 +1,7 @@
+class ConsultationPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
