597300c0b0a5b17bdfdb56e9a785e5a69eca93ca
Emmanuel BIRABEN <ebiraben14@hotmail.com>
2021-05-25 11:25:08 +0200

    colomn a jour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_091456) do
+ActiveRecord::Schema.define(version: 2021_05_25_092120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_05_25_091456) do
     t.bigint "doctor_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.time "time"
     t.index ["doctor_id"], name: "index_consultations_on_doctor_id"
     t.index ["user_id"], name: "index_consultations_on_user_id"
   end
@@ -59,7 +60,6 @@ ActiveRecord::Schema.define(version: 2021_05_25_091456) do
     t.integer "weight"
     t.string "allergies"
     t.string "treatments"
-    t.time "time"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
