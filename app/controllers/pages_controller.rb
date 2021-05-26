846e2b017efde555f01e18968119e3174e68ebb5
saskia aardenburg <saskia.aardenburg@gmail.com>
2021-05-26 16:03:11 +0200

    travail en cours

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    # @consultation = Consultation.find(params[:id])
+    @consultation = Consultation.where('date > ?', Date.today).first
+    @scrolling_days = (Date.today.beginning_of_week..Date.today.end_of_week + 14.days).to_a
+    @consultation_soon = Consultation.where('date > ?', Date.today)
+    # @consultation = Consultation.where('at > ?', DateTime.now).first
   end
 
   def timeline
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def random_conseil
+    [
+      "Buvez 2 litres d\'eau/jour",
+      "Essayez de faire 10.000 pas/jour",
+      "Pratiquez une activité physique régulière",
+      "Evitez de manger trop gras/sucré/salé",
+      "Pensez à manger 5 fruits et légumes/ jour",
+      "Essayez de dormir 8h/nuit",
+      "Mangez des fruits et légumes de saison",
+      "Prenez du temps pour vous détendre",
+      "Evitez de consommer régulièrement de l'alcool",
+      "Evitez les écrans avant de vous coucher"
+    ].sample
+  end
 end
