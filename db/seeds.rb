dfddb06cb2c390ff91e4fda3f9e8d8bee33fede7
Oceane Delain <delain.oceane@gmail.com>
2021-05-31 16:48:19 +0200

    ordered v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@ require "open-uri"
 
 puts "Cleaning database"
 
-Document.destroy_all
-Consultation.destroy_all
 Symptom.destroy_all
 Doctor.destroy_all
+Document.destroy_all
+Consultation.destroy_all
 User.destroy_all
 
 puts "Database cleaned, now create doctors"
