8549e61f4b93589907829ca8f484d0497ba5e3c3
saskia aardenburg <saskia.aardenburg@gmail.com>
2021-06-01 12:25:15 +0200

    ouloulou

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,23 @@
+
+// SCROLL HEADER
+window.onscroll = function () { myFunction() };
+
+// Get the header
+var navbar = document.getElementById("myHeader", "navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+
+
