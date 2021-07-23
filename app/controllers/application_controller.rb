5a7aa132af78f57a756a0ce828424ee9ebb5f99c
Rachel Nascimento <rachelle.nascimento@gmail.com>
2021-07-23 17:57:17 +0200

    add avatar for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
   end
   
   private
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :landing
 
   def landing
+    if current_user
+      redirect_to home_path
+    else
+      root_path
+    end
   end
 
   def home
@@ -23,8 +28,10 @@ else
       @data = @data.select { |pg_doc| pg_doc.searchable_type == "Consultation" }
     end
 
-    params[:focus] ||= "consultation_#{current_user.next_consultation.id}"
-    @data = @data.map(&:searchable).sort_by { |item| item.at }
+    if current_user.consultations.any?
+      params[:focus] ||= "consultation_#{current_user.next_consultation.id}"
+      @data = @data.map(&:searchable).sort_by { |item| item.at }
+    end
 
     respond_to do |format|
       format.html # Follow regular flow of Rails
